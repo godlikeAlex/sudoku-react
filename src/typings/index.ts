bf77c675d4f95be810c5c9eb95eeec03e1ad2ce1
Yurkovsky <godlikedesigner@gmail.com>
2020-07-17 01:52:49 +0500

    added type for utils and shuffle

---
+++ src/typings/index.ts
@@ -0,0 +1,7 @@
+export type Grid = [Row, Row, Row, Row, Row, Row, Row, Row, Row];
+
+export type N = 0 | Numbers;
+
+export type Numbers = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
+
+export type Row = [N, N, N, N, N, N, N, N, N];
